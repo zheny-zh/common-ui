6449b3d5e42d842c980b9259a61aac75129f8801
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-26 12:32:39 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
-      outDir: "dist",
-      entryRoot: "src",
       insertTypesEntry: true,
-      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: ["src/env.ts", "src/**/*.stories.ts"],
     }),
   ],
 
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: "src/index.ts",
       name: "common-ui",
       fileName: (format) => `common-ui.${format}.js`,
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
       external: ["react"],

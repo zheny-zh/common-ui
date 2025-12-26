dcf69ba4516b77ce09d141c3902f01d8d6d12307
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-26 13:34:11 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
@@ -17,14 +18,26 @@ export default defineConfig({
     lib: {
       entry: "src/index.ts",
       name: "common-ui",
-      fileName: (format) => `common-ui.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
       formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
-      external: ["react"],
+      external: [
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/react-fontawesome",
+        "motion",
+        "react",
+        "react-bootstrap",
+        "react-dom",
+      ],
       output: {
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
+          "react-bootstrap": "ReactBootstrap",
+          "@fortawesome/react-fontawesome": "FontAwesomeReact",
+          "@fortawesome/free-solid-svg-icons": "FontAwesomeFreeSolidIcon",
+          motion: "motion",
         },
       },
     },
